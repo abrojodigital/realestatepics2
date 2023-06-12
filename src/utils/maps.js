ce41b394679d7a1d2e682507879d9005620a9df0
abrojodigital <abrojodigital@gmail.com>
2023-06-12 15:25:20 -0300

    Quito API_KEY por seguridad

--- src/utils/maps.js
+++ src/utils/maps.js
@@ -1,4 +1,4 @@
-export const MAPS_API_KEY = "AIzaSyAlSD5v6-Zd4YVBSvLB5NR74_M5IFeMM44";
+export const MAPS_API_KEY = "";
 
 export const URL_MAPS = (lat, lng, zoom = 14) =>
   `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap
